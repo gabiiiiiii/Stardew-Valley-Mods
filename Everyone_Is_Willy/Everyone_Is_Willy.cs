6ece43f14fba8bb358289e6f921638f290d70952
Austin Stroupe <austin.stroupe.m@gmail.com>
2021-03-29 21:55:58 -0400

    Code has been cleaned up. Still not exactly perfect, but much much much better.

--- Everyone_Is_Willy/Everyone_Is_Willy.cs
+++ Everyone_Is_Willy/Everyone_Is_Willy.cs
@@ -19,7 +19,29 @@ namespace Everyone_is_Willy
 
         public bool CanLoad<T>(IAssetInfo asset)
         {
+            String[] characters =
+                {"Abigail", "Abigail_Beach", "Alex", "Alex_Beach", "Baby", "Baby_dark",
+                "Caroline", "Caroline_Beach", "Clint", "Clint_Beach", "Dana", "Demetrius",
+                "Demetrius_Beach", "Dwarf", "Elliott", "Elliott_Beach", "Emily", "Emily_Beach",
+                "Evelyn", "George", "Gus", "Haley", "Haley_Beach", "Harvey", "Harvey_Beach",
+                "Jas", "Jodi", "Jodi_Beach", "Kent", "Leah", "Leah_Beach", "Lewis", "Lewis_Beach",
+                "Linus", "Marcello", "Marlon", "Marnie", "Marnie_Beach", "Maru", "Maru_Beach",
+                "Maru_Hospital", "Morris", "Pam", "Pam_Beach", "Penny", "Penny_Beach", "Pierre",
+                "Pierre_Beach", "Robin", "Robin_Beach", "Sam", "Sam_Beach", "Sandy", "Sebastian",
+                "Sebastian_Beach", "Shane", "Shane_Beach", "Shane_JojaMart", "Toddler", "Toddler_dark",
+                "Toddler_girl", "Toddler_girl_dark", "Vincent", "Wizard"};
+
+            for (int i = 0; i < characters.Length; i++)
+            {
+                String charName = "Characters/" + characters[i];
+                String porName = "Portraits/" + characters[i];
+                if (asset.AssetNameEquals(charName) || asset.AssetNameEquals(porName))
+                {
+                    return true;
+                }
+            }
             //Abigail
+            /*
             if (asset.AssetNameEquals("Characters/Abigail") || asset.AssetNameEquals("Portraits/Abigail"))
             {
                 return true;
@@ -234,6 +256,7 @@ namespace Everyone_is_Willy
             {
                 return true;
             }
+            */
 
             return false;
         }
@@ -242,6 +265,7 @@ namespace Everyone_is_Willy
         {
 
             // Abigail
+            /*
             if (asset.AssetNameEquals("Characters/Abigail"))
             {
                 return this.Helper.Content.Load<T>("Characters/Willy", ContentSource.GameContent);
@@ -630,6 +654,34 @@ namespace Everyone_is_Willy
             if (asset.AssetNameEquals("Characters/Baby"))
             {
                 return this.Helper.Content.Load<T>("Characters/Willy", ContentSource.GameContent);
+            }
+            */
+
+            String[] characters =
+                {"Abigail", "Abigail_Beach", "Alex", "Alex_Beach", "Baby", "Baby_dark",
+                "Caroline", "Caroline_Beach", "Clint", "Clint_Beach", "Dana", "Demetrius",
+                "Demetrius_Beach", "Dwarf", "Elliott", "Elliott_Beach", "Emily", "Emily_Beach",
+                "Evelyn", "George", "Gus", "Haley", "Haley_Beach", "Harvey", "Harvey_Beach",
+                "Jas", "Jodi", "Jodi_Beach", "Kent", "Leah", "Leah_Beach", "Lewis", "Lewis_Beach",
+                "Linus", "Marcello", "Marlon", "Marnie", "Marnie_Beach", "Maru", "Maru_Beach",
+                "Maru_Hospital", "Morris", "Pam", "Pam_Beach", "Penny", "Penny_Beach", "Pierre",
+                "Pierre_Beach", "Robin", "Robin_Beach", "Sam", "Sam_Beach", "Sandy", "Sebastian",
+                "Sebastian_Beach", "Shane", "Shane_Beach", "Shane_JojaMart", "Toddler", "Toddler_dark",
+                "Toddler_girl", "Toddler_girl_dark", "Vincent", "Wizard"};
+
+            for (int i = 0; i < characters.Length; i++)
+            {
+                String charName = "Characters/" + characters[i];
+                String porName = "Portraits/" + characters[i];
+                if (asset.AssetNameEquals(charName))
+                {
+                    return this.Helper.Content.Load<T>("Characters/Willy", ContentSource.GameContent);
+                }
+
+                if (asset.AssetNameEquals(porName))
+                {
+                    return this.Helper.Content.Load<T>("Portraits/Willy", ContentSource.GameContent);
+                }
             }
 
             throw new InvalidOperationException($"Unexpected asset '{asset.AssetName}'.");
