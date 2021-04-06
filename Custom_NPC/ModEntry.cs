94b91d7e528c94a54e2b2ee562d0bea2b4ee1402
Austin Stroupe <austin.stroupe.m@gmail.com>
2021-04-06 15:57:58 -0400

    Dialogue/Schedule should be fine. Need to add sprite, protrait, and need to spawn npc

--- Custom_NPC/ModEntry.cs
+++ Custom_NPC/ModEntry.cs
@@ -11,6 +11,8 @@ namespace Custom_NPC
     /// <summary>The mod entry point.</summary>
     public class ModEntry : Mod, IAssetLoader, IAssetEditor
     {
+        static int x = 1;
+
         public override void Entry(IModHelper helper)
         {
 
@@ -18,26 +20,59 @@ namespace Custom_NPC
 
         public bool CanLoad<T>(IAssetInfo asset)
         {
-            return asset.AssetNameEquals("Charactesrs/Dialogue/Spoop_Dog");
-            return asset.AssetNameEquals("Characters/schedules/Spoop_Dog");
-            return asset.AssetNameEquals("Characters/Spoop_Dog");
-            return asset.AssetNameEquals("Portraits/Spoop_Dog");
+            switch(x)
+            {
+                case 1:
+                    x++;
+                    return asset.AssetNameEquals("Charactesrs/Dialogue/Spoop_Dog");
+                case 2:
+                    x++;
+                    return asset.AssetNameEquals("Characters/schedules/Spoop_Dog");
+                case 3:
+                    x++;
+                    return asset.AssetNameEquals("Mods/Assets/Sprite/Spoop_Dog");
+                case 4:
+                    x++;
+                    return asset.AssetNameEquals("Mods/assets/Portrait/Spoop_Dog");
+                default:
+                    return false;
+            }
+
+            //return asset.AssetNameEquals("Charactesrs/Dialogue/Spoop_Dog");
+            //return asset.AssetNameEquals("Characters/schedules/Spoop_Dog");
+            //return asset.AssetNameEquals("Mods/Assets/Sprite/Spoop_Dog");
+            //return asset.AssetNameEquals("Mods/assets/Portrait/Spoop_Dog");
         }
 
         
         public T Load<T>(IAssetInfo asset)
         {
-            return (T)(object)new Dictionary<string, string>
+            if (asset.AssetNameEquals("Charactesrs/Dialogue/Spoop_Dog"))
             {
-                ["Introduction"] = "BOOoOooOoOooOoOooOoOo"
-            };
-
-            return (T)(object)new Dictionary<string, string>
+                return (T)(object)new Dictionary<string, string>
+                {
+                    ["Introduction"] = "BOOoOooOoOooOoOooOoOo"
+                };
+            }
+            else if (asset.AssetNameEquals("Characters/schedules/Spoop_Dog"))
             {
-
-            };
-
-            
+                return (T)(object)new Dictionary<string, string>
+                {
+                    ["spring"] = "600 Town 45 88 2"
+                };
+            }
+            else if (asset.AssetNameEquals("Mods/Assets/Sprite/Spoop_Dog"))
+            {
+                return;
+            }
+            else if (asset.AssetNameEquals("Mods/Assets/Portrait/Spoop_Dog"))
+            {
+                return;
+            }
+            else
+            {
+                throw new NotImplementedException("Ya broke it");
+            }
         }
 
         public bool CanEdit<T>(IAssetInfo asset)
